83b5f2ba0b90315a1b9db1f16b51eb3b288646bc
NelliWel <Nelli.Welker@haw-hamburg.de>
2018-04-27 15:38:17 +0200

    Main-Klasse

--- src/itsp02/LCG.java
+++ src/itsp02/LCG.java
@@ -14,25 +14,6 @@ public class LCG {
 		this.x = x;
 	}
 	
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	
-	public int getX() {
-		return x;
-	}
-
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
 	/*
 	 * Erstellt eine Zufallszahl nach der linearen Kongruenzmethode
 	 * 
