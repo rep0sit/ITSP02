d59f3ee10bc7f6a1e7f19da4b7799380e4bd3c80
NelliWel <Nelli.Welker@haw-hamburg.de>
2018-04-27 14:39:43 +0200

    Initiales Projektgerüst

---
+++ src/itsp02/LCG.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package itsp02;
+
+/**
+ * @author È. Onasch, N. Welker
+ *
+ */
+public class LCG {
+	private int x0;
+	
+	public LCG(int x0){
+		this.x0 = x0;
+	}
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public int getX0() {
+		return x0;
+	}
+
+
+	public void setX0(int x0) {
+		this.x0 = x0;
+	}
+
+
+	public int nextInt(){
+		//TODO
+		return 0;
+	}
+}
