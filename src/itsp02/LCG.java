2bf3ac01846c193b8a83d9e911b18b5bbca1aa4b
etienne0n <etienneonasch@web.de>
2018-04-28 14:40:40 +0200

    LCG Klasse fertig. LCGTest (Aufg. 2b) fertig.

--- src/itsp02/LCG.java
+++ src/itsp02/LCG.java
@@ -9,19 +9,54 @@ package itsp02;
  *
  */
 class LCG {
+	
+	/*
+	 * +++++++++++++++++++
+	 * 		CONSTANTS
+	 * +++++++++++++++++++
+	 */
+	
+	/*
+	 * Parameterkombinationen fuer das lineare Kongruenzverfahren
+	 * nach
+	 * Berkeley UNIX Pascal
+	 */
+	
+	private static final long A = 62605;
+	private static final long B = 113218009;
+	private static final long N = 536870912; // 2^29
+	
+	
+	/*
+	 * +++++++++++++++++++
+	 * 		Variables
+	 * +++++++++++++++++++
+	 */
+	
 	private int x;
+	
+	/*
+	 * +++++++++++++++++++
+	 * 		Construction
+	 * +++++++++++++++++++
+	 */
 	
 	public LCG(int x){
 		this.x = x;
 	}
 	
 	/*
+	 * +++++++++++++++++++
+	 * 		Methods
+	 * +++++++++++++++++++
+	 */
+	
+	/*
 	 * Erstellt eine Zufallszahl nach der linearen Kongruenzmethode
 	 * 
 	 */
 	public int nextInt(){
-		//TODO
-		
-		return 0;
+		this.x = (int) ((A * this.x + B) % N);
+		return this.x;
 	}
 }
--- testSrc/itsp02/LCGTest.java
+++ testSrc/itsp02/LCGTest.java
@@ -5,10 +5,36 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCGTest {
-
+	/*
+	 * +++++++++++++++++++
+	 * 		CONSTANTS
+	 * +++++++++++++++++++
+	 */
+	
+	private static final int BYTE = 256;
+	
+	/*
+	 * +++++++++++++++++++
+	 * 		TESTS
+	 * +++++++++++++++++++
+	 */
+	
 	@Test
 	public void testNextInt() {
-		fail("Not yet implemented");
+		int startWert = (int) (Math.random() * 10000);
+		
+		LCG lcg = new LCG(startWert);
+		
+		for(int i = 1; i <= BYTE; i++) {
+			
+			int nextFirstByte = lcg.nextInt() & (0x000000FF); 
+			
+			assertTrue(nextFirstByte >= 0 && nextFirstByte < BYTE);
+			
+			System.out.println(i + ". Zahl = " + nextFirstByte);
+		}
+		
+		System.out.println("Der Startwert war: " + startWert);
 	}
 
 }
