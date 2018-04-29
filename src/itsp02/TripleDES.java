1e3ef364117d34b19b90abc27553e66ea9065157
etienne0n <etienneonasch@web.de>
2018-04-29 13:56:39 +0200

    Encrypt in TripleDES fertig

--- src/itsp02/TripleDES.java
+++ src/itsp02/TripleDES.java
@@ -8,6 +8,18 @@ public final class TripleDES {
 	private TripleDES() {}
 	
 	private static void encrypt(byte[]key1, byte[]key2, byte[]key3, byte[]source, byte[]target) {
+		DES des1 = new DES(key1);
+		DES des2 = new DES(key2);
+		DES des3 = new DES(key3);
+		
+		
+		byte[] first = new byte[8];
+		byte[] second = new byte[8];
+		
+		des1.encrypt(source, 0, first, 0);
+		des2.decrypt(first, 0, second, 0);
+		des3.encrypt(second, 0, target, 0);
+		
 		
 	}
 	
