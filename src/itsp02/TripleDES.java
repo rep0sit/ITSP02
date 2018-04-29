aaf99cc9f65c8fe6f8563438361c4452a2563f0b
etienne0n <etienneonasch@web.de>
2018-04-29 13:50:57 +0200

    parameter IV entfernt

--- src/itsp02/TripleDES.java
+++ src/itsp02/TripleDES.java
@@ -7,14 +7,16 @@ package itsp02;
 public final class TripleDES {
 	private TripleDES() {}
 	
-	private static void encrypt(byte[]key1, byte[]key2, byte[]key3, byte[]iv, byte[]source, byte[]target) {
+	private static void encrypt(byte[]key1, byte[]key2, byte[]key3, byte[]source, byte[]target) {
 		
 	}
 	
 	
-	private static void decrypt(byte[]key1, byte[]key2, byte[]key3, byte[]iv, byte[]source, byte[]target) {
+	private static void decrypt(byte[]key1, byte[]key2, byte[]key3, byte[]source, byte[]target) {
 		
 	}
+	
+	
 	
 	
 	public static void main(String...args) {
